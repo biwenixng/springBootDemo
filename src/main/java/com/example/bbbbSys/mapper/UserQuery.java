fc331fd825878b55b47f36708b65a8d310138258
biwenxing <1350500764@qq.com>
2023-02-19 20:54:55 +0800

    登录

---
+++ src/main/java/com/example/bbbbSys/mapper/UserQuery.java
@@ -0,0 +1,24 @@
+package com.example.bbbbSys.mapper;
+
+import com.example.bbbbSys.pojo.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ *
+ * @author Administrator
+ */
+
+@Mapper
+@Repository
+public interface UserQuery {
+    /**
+     * 查询用户
+     * @param user
+     * @return
+     */
+    List<User> loginUser(User user);
+}
